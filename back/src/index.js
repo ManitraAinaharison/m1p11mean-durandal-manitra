50ccf4fda814c694c9a40e535b26839c5cd6315d
Anthony Durandal <a.durandal@outlook.com>
2024-02-04 21:45:19 +0300

    setup[back]: add mongo env variables
    install dotenv
    create separate environments for dev and prod
    test connection with mongo dev remote db

--- back/src/index.js
+++ back/src/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env" : ".env.dev",
+});
+
 const app = express();
 const port = 5000;
 
@@ -6,6 +12,21 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+mongoose.connect(process.env.MONGO_STRING_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, // Enables the new unified topology engine (recommended for future compatibility).
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 });
