fc115a742f7c1ad5ff4cfe3ed56a0f6271cb97b8
Anthony Durandal <a.durandal@outlook.com>
2024-02-07 19:23:35 +0300

    feat[back]: login service

--- manja-backend/src/api/v1/auth/services/authentication.service.js
+++ manja-backend/src/api/v1/auth/services/authentication.service.js
@@ -1,36 +1,11 @@
 const User = require("../schemas/user.schema").User;
 const ROLES = require("../schemas/user.schema").ROLES;
-const jwt = require("jsonwebtoken");
-
-// HS256 by default
-async function createToken(payload, { expiresIn, ...otherOptions }) {
-  return jwt.sign(payload, process.env.API_SECRET, {
-    expiresIn,
-    ...otherOptions,
-  });
-}
-
-async function createAccessToken({ username, email, role }) {
-  return createToken(
-    { username, email, role },
-    { expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10) }
-  );
-}
-
-async function createRefreshToken({ username, email, role }) {
-  return createToken(
-    { username, email, role },
-    { expiresIn: parseInt(process.env.REFRESH_TOKEN_EXPIRATION, 10) }
-  );
-}
-
-async function generateTokens({ username, email, role }) {
-  const accessToken = createAccessToken({ username, email, role });
-  const refreshToken = createRefreshToken({ username, email, role });
-  return { accessToken, refreshToken };
-}
+const mongoose = require("mongoose");
+const seecurityUtil = require("../../../../util/security.util");
 
 module.exports.register = async function register(req) {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
     const { firstname, lastname, username, password, email, role } = req.body;
     let newUser = await User.create({
@@ -38,24 +13,55 @@ module.exports.register = async function register(req) {
       lastname,
       username,
       password,
-      email : email.toLowerCase(),
-      role : ROLES.USER,
+      email: email.toLowerCase(),
+      role: ROLES.USER,
     });
 
-    newUser = await newUser.save();
-    const { accessToken, refreshToken } = generateTokens({
+    newUser = await newUser.save({ session });
+    const { accessToken, refreshToken } = securityUtil.generateTokens({
       username,
       email,
       role,
     });
-
+    await session.commitTransaction();
     return {
       accessToken,
       refreshToken,
       responseBody: { username, email, role },
     };
   } catch (e) {
-    console.log(e)
+    await session.abortTransaction();
+    console.log(e);
     throw new Error("Veuillez réessayer s'il vous plaît");
+  } finally {
+    await session.endSession();
   }
+};
+
+module.exports.login = async function login(req) {
+  try {
+    const { email, password } = req.body;
+    const user = User.findOne({
+      email,
+      password: seecurityUtil.hash(password),
+    });
+    if (!user) return null;
+    const { accessToken, refreshToken } = securityUtil.generateTokens({
+      username,
+      email,
+      role: user.role,
+    });
+    return {
+      accessToken,
+      refreshToken,
+      responseBody: { username, email, role },
+    };
+  } catch (e) {
+    console.log(e);
+    throw new Exception(e.message);
+  }
+};
+
+module.exports.listUsers = async function register() {
+  return await User.find();
 };
