1ef4b362a6d9e8fb2e021a6355883b082005a717
Anthony Durandal <a.durandal@outlook.com>
2024-02-07 19:29:03 +0300

    feat: login route

--- manja-backend/src/api/v1/auth/controllers/auth.controller.js
+++ manja-backend/src/api/v1/auth/controllers/auth.controller.js
@@ -2,19 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 const authenticationService = require("../services/authentication.service");
+const authHelper = require('../helpers/auth.helper')
 
 router.post("/register", (req, res) => {
-  const { accessToken, refreshToken, responseBody } =
-    authenticationService.register(req);
-  res.cookie("jwt", accessToken, {
-    httpOnly: true,
-    maxAge: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10),
-  });
-  res.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
-    maxAge: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10),
-  });
-  res.status(201).json(responseBody);
+  try {
+    const { accessToken, refreshToken, responseBody } =
+      authenticationService.register(req);
+    authHelper.addTokenCookies(res, { accessToken, refreshToken});
+    res.status(201).json(responseBody);
+  } catch (e) {
+    console.log(e.message)
+    res.status(500, "An error occured  please try again");
+  }
 });
+
+router.post("/login", (req, res) => {
+  try {
+    const { accessToken, refreshToken, responseBody } =
+      authenticationService.login(req);
+    authHelper.addTokenCookies(res, { accessToken, refreshToken });
+    res.status(201).json(responseBody);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500, "An error occured  please try again");
+  }
+});
+
+router.get("/users", (req, res)=>{
+  
+  return res
+    .status(200)
+    .json(authenticationService.listUsers());
+})
 
 module.exports = router;
