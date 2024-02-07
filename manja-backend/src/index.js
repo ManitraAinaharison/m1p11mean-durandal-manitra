d1ef8d3c50a52c9388a60aa1b883f7c95de6701b
Anthony Durandal <a.durandal@outlook.com>
2024-02-07 12:26:20 +0300

    refactor config imports + add body-parser

--- manja-backend/src/index.js
+++ manja-backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 require("dotenv").config({
   path: process.env.NODE_ENV === "production" ? ".env" : ".env.dev",
@@ -12,15 +13,17 @@ const db = mongoose.connection;
 const app = express();
 const port = process.env.PORT;
 
-const routes = require('./config/routes.config') // routes changes based on API version
+const routes = require('./config/routes.config') // routes are based on API version
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", routes);
 
 db.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
 
 db.once("open", () => {
-  app.use('/', routes)
-
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
