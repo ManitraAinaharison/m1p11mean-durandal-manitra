dab816855af8c1925062ae81ee0426e688cd8ce3
Anthony Durandal <a.durandal@outlook.com>
2024-02-08 14:55:57 +0300

    feat[back] : add auth middleware

--- manja-backend/src/api/v1/auth/helpers/auth.helper.js
+++ manja-backend/src/api/v1/auth/helpers/auth.helper.js
@@ -1,43 +1,29 @@
 const securityUtil = require("../../../../util/security.util");
 
-module.exports.addTokenCookies = function addTokenCookies(
+function addTokenCookies(
   response,
   { accessToken, refreshToken }
 ) {
   response.cookie("accessToken", accessToken, {
-    httpOnly: true,
+    // httpOnly: true,
+    secure: true,
+    sameSite: 'none',
     maxAge: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10),
+    maxAge: 60,
   });
   response.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
+    // httpOnly: true,
+    secure: true,
+    sameSite: "none",
     maxAge: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10),
+    maxAge: 60,
   });
 };
 
-module.exports.authorise = function authorise(permittedRoles) {
-  return (req, res, next) => {
-    const accessToken = req.cookies.accessToken;
-    const refreshToken = req.cookies.refreshToken;
-
-    const decodedAccesToken = securityUtil.decodeToken(accessToken);
-    const decodedRefreshToken = securityUtil.decodeToken(refreshToken);
-
-    if (!permittedRoles.include(decodedAccesToken.role)) {
-      // throw new Error("not enough permission for this route");
-      res.redirect("/login", "access denied");
-    } else if (
-      securityUtil.isTokenExpired(decodedAccesToken) &&
-      securityUtil.isTokenExpired(decodedRefreshToken)
-    ) {
-      res.redirect("/login", "access denied");
-    } else if (
-      securityUtil.isTokenExpired(decodedAccesToken) &&
-      !securityUtil.isTokenExpired(decodedRefreshToken)
-    ){
-        const {accessToken, refreshToken} = generateTokens(decodedAccesToken.username, decodedAccesToken.email, decodedAccesToken.role)
-        addTokenCookies()
-    }
-      next();
-  };
+function removeTokenCookies(response) {
+  response.clearCookie("accessToken");
+  response.clearCookie("refreshToken");
 };
 
+module.exports.addTokenCookies = addTokenCookies;
+module.exports.removeTokenCookies = removeTokenCookies;
