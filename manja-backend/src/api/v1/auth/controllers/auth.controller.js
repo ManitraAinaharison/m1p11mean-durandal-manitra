b6b1b6a0438662165603a983d451fd798c7b4f4f
Anthony Durandal <a.durandal@outlook.com>
2024-02-08 14:55:06 +0300

    debug: auth function + add remove test function for protected routes
    add async

--- manja-backend/src/api/v1/auth/controllers/auth.controller.js
+++ manja-backend/src/api/v1/auth/controllers/auth.controller.js
@@ -2,37 +2,41 @@ const express = require("express");
 const router = express.Router();
 
 const authenticationService = require("../services/authentication.service");
-const authHelper = require('../helpers/auth.helper')
+const authHelper = require("../helpers/auth.helper");
+const authMiddleware = require("../middlewares/auth.middleware");
+const { ROLES } = require("../schemas/user.schema");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { accessToken, refreshToken, responseBody } =
-      authenticationService.register(req);
-    authHelper.addTokenCookies(res, { accessToken, refreshToken});
-    res.status(201).json(responseBody);
-  } catch (e) {
-    console.log(e.message)
-    res.status(500, "An error occured  please try again");
-  }
-});
-
-router.post("/login", (req, res) => {
-  try {
-    const { accessToken, refreshToken, responseBody } =
-      authenticationService.login(req);
-    authHelper.addTokenCookies(res, { accessToken, refreshToken });
+      await authenticationService.register(req);
+    await authHelper.addTokenCookies(res, { accessToken, refreshToken });
     res.status(201).json(responseBody);
   } catch (e) {
     console.log(e.message);
-    res.status(500, "An error occured  please try again");
+    res.sendStatus(500, "An error occured  please try again");
   }
 });
 
-router.get("/users", (req, res)=>{
-  
-  return res
-    .status(200)
-    .json(authenticationService.listUsers());
-})
+router.post("/login", async (req, res) => {
+  try {
+    const { accessToken, refreshToken, responseBody } =
+      await authenticationService.login(req);
+    console.log(accessToken, refreshToken)
+    await authHelper.addTokenCookies(res, { accessToken, refreshToken });
+    res.status(201).json(responseBody);
+  } catch (e) {
+    console.log(e.message);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/users", (req, res) => {
+  return res.status(200).json(authenticationService.listUsers());
+});
+
+router.get("/users-protected", authMiddleware.authorise([ROLES.EMPLOYEE]), (req, res) => {
+  return res.status(200).json(authenticationService.listUsers());
+});
 
 module.exports = router;
