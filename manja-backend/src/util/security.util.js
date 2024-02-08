f653fac2ba5a8503256b76e71ea0e1ae061290f0
Anthony Durandal <a.durandal@outlook.com>
2024-02-08 14:56:37 +0300

    feat: check functions for token expiration

--- manja-backend/src/util/security.util.js
+++ manja-backend/src/util/security.util.js
@@ -1,45 +1,65 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-module.exports.hash = function hash(value) {
-  return bcrypt.hashSync(value);
-};
-
-module.exports.decodeToken = function decodeToken(value){
-    return jwt.decode(token);
+function hash(value) {
+  const salt = bcrypt.genSaltSync(parseInt(process.env.HASH_SALT_ROUNDS));
+  return bcrypt.hashSync(value, salt);
 }
 
-module.exports.isTokenExpired = function isTokenExpired(token) {
-    const decoded = decodeToken(token); // Decoded payload (no verification)
-    console.log('expiration Timestamp : ', decoded.exp);
-    const expirationTimestamp = decoded.exp * 1000; // Convert to milliseconds
-    return Date.now() >= expirationTimestamp;
-};
+function isMatch(data, hashedData) {
+  return bcrypt.compareSync(data, hashedData);
+}
+
+function decodeToken(token) {
+  return jwt.decode(token, process.env.API_SECRET);
+}
+
+async function isTokenExpired(token, orTokenIsNull=false) {
+  if(!token && orTokenIsNull === true) return true;
+  const decoded = decodeToken(token);
+  const expirationTimestamp = decoded.exp * 1000; // Convert to milliseconds
+  return Date.now() >= expirationTimestamp;
+}
 
 // HS256 by default
-module.exports.createToken = async function createToken(payload, { expiresIn, ...otherOptions }) {
-  return jwt.sign(payload, process.env.API_SECRET, {
+async function createToken(payload, { expiresIn, ...otherOptions }) {
+  return await jwt.sign(payload, process.env.API_SECRET, {
     expiresIn,
     ...otherOptions,
   });
 }
 
-module.exports.createAccessToken = async function createAccessToken({ username, email, role }) {
-  return createToken(
+async function createAccessToken({ username, email, role }) {
+  return await createToken(
     { username, email, role },
     { expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRATION, 10) }
   );
 }
 
-module.exports.createRefreshToken = async function createRefreshToken({ username, email, role }) {
-  return createToken(
+async function createRefreshToken({ username, email, role }) {
+  return await createToken(
     { username, email, role },
     { expiresIn: parseInt(process.env.REFRESH_TOKEN_EXPIRATION, 10) }
   );
 }
 
-module.exports.generateTokens = async function generateTokens({ username, email, role }) {
-  const accessToken = createAccessToken({ username, email, role });
-  const refreshToken = createRefreshToken({ username, email, role });
+async function generateTokens({ username, email, role }) {
+  const accessToken = await createAccessToken({ username, email, role });
+  const refreshToken = await createRefreshToken({ username, email, role });
   return { accessToken, refreshToken };
 }
+
+async function sameUserToken(decodedAccessToken, decodedRefreshToken){
+  const fields = ["username", "email", "role"];
+  return fields.every((field)=>decodedAccessToken[field] === decodedRefreshToken[field])
+}
+
+module.exports.hash = hash;
+module.exports.isMatch = isMatch;
+module.exports.decodeToken = decodeToken;
+module.exports.isTokenExpired = isTokenExpired;
+module.exports.createToken = createToken;
+module.exports.createAccessToken = createAccessToken;
+module.exports.createRefreshToken = createRefreshToken;
+module.exports.generateTokens = generateTokens;
+module.exports.sameUserToken = sameUserToken;
