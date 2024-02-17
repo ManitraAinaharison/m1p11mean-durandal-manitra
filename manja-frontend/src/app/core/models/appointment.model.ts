9d4664367c975cf67a82ffe86cd6ee0881a83cf8
Anthony Durandal <a.durandal@outlook.com>
2024-02-17 10:59:39 +0300

    feat[service]: create appointment service

---
+++ manja-frontend/src/app/core/models/appointment.model.ts
@@ -0,0 +1,34 @@
+import { Dayjs } from "dayjs";
+import { ServiceModel } from "./salon-service.model";
+import { User } from "./user.model";
+
+export interface Appointment {
+  appointmentDate: Date;
+  client: User;
+  employee: User;
+  subService: ServiceModel;
+  price: number;
+  reminders: []; // LOW
+  payment: {
+    paymentDate: Date;
+    amount: number;
+  };
+  status: number; // 0:
+  commision: number;
+  statusHistory: [
+    {
+      status: number;
+      statusDate: Date;
+    }
+  ];
+}
+
+export interface DateInterval {
+  start: Dayjs,
+  end: Dayjs 
+}
+
+export interface DateIntervalDetails extends DateInterval {
+  dailyPercentage: number;
+  duration: number,
+}
