9814379f11d3bf0da323f260b926b274d4d4f304
Anthony Durandal <a.durandal@outlook.com>
2024-02-17 11:02:11 +0300

    feat[component]: add selectionable subservice in reservation page

--- manja-frontend/src/app/features/service/pages/reservation/reservation.component.ts
+++ manja-frontend/src/app/features/service/pages/reservation/reservation.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SalonService } from '../../../../core/services/salon-service.service';
-import { ServiceModel, SubService } from '../../../../core/models/salon-service.model';
+import {
+  ServiceModel,
+  SubService,
+} from '../../../../core/models/salon-service.model';
+import { AppointmentService } from '../../../../core/services/appointment.service';
+import { EmployeeService } from '../../../../core/services/employee.service';
+import { Employee } from '../../../../core/models/user.model';
+import dayjs, { Dayjs } from 'dayjs';
+import { DateInterval, DateIntervalDetails } from '../../../../core/models/appointment.model';
+import { toDateIntervalDetails } from '../../../../core/util/date.util';
 
 @Component({
   selector: 'app-reservation',
@@ -11,23 +20,58 @@ import { ServiceModel, SubService } from '../../../../core/models/salon-service.
 export class ReservationComponent {
   serviceSlug: string | null = null;
   matchingService: ServiceModel | null = null;
+  selectedSubService: SubService | null = null;
+  employees: Employee[] | null = null;
+  nonAvailableHours: DateIntervalDetails[] = [];
+  referenceDate: Dayjs = dayjs();
+  openingHour : number = 9;
+  closingHour : number = 17;
+
   constructor(
     private route: ActivatedRoute,
-    private salonService: SalonService
+    private salonService: SalonService,
+    private appointmentService: AppointmentService,
+    private employeeService: EmployeeService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       let slug: string | null = params.get('sub-service-slug');
-      this.serviceSlug = slug
+      this.serviceSlug = slug;
       this.setMatchingService(slug);
+      this.selectedSubService;
     });
+    this.employeeService.getEmployees().subscribe((employeeList) => {
+      this.setEmployees(employeeList);
+    });
+    this.appointmentService
+      .getNonAvailableHours(this.referenceDate)
+      .subscribe((hours) => {
+        this.setNonAvailableHours(hours);
+      });
   }
 
   setMatchingService(slug: string | null) {
-    if (!slug) return ;
+    if (!slug) return;
     this.salonService
       .getService(slug)
       .subscribe((service) => (this.matchingService = service)); // handle if getSubService returns null
+  }
+
+  getEmployees() {
+    this.employeeService.getEmployees();
+  }
+
+  setEmployees(value: Employee[]): void {
+    this.employees = value;
+  }
+
+  setSelectedSubService(selected: SubService): void {
+    this.selectedSubService = selected;
+  }
+
+  setNonAvailableHours(hours: DateInterval[]): void {
+    this.nonAvailableHours = toDateIntervalDetails(hours, (this.closingHour - this.openingHour) * 60);
+    console.log(this.nonAvailableHours)
   }
 }
