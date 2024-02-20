dff2edd05a38021ff067750444fbf1a57d9112a1
ARTI <manitraainaharison@gmail.com>
2024-02-21 02:58:44 +0300

    Dashboard template (without navbar):
    - Creating the dashboard layout
    - Creation of sidebar
    - Collapse sidebar
    - Add animation to sidebar
    - Integrate tooltips into sidebar item when compressed

--- manja-frontend/src/app/shared/directives/tooltip.directive.ts
+++ manja-frontend/src/app/shared/directives/tooltip.directive.ts
@@ -1,37 +1,41 @@
 import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 interface TooltipConfig {
+    show: boolean;
     text: string;
     position?: 'top' | 'right' | 'bottom' | 'left';
 }
 
 @Directive({
-    selector: '[appTooltip]'
+    selector: '[tooltip]',
+    standalone: true,
 })
 export class TooltipDirective {
 
     @Input() tooltipConfig: TooltipConfig = {
+        show: true,
         text: "",
         position: "top"
     };
     tooltipElement: HTMLDivElement | null = null;
-    
+
     constructor(
-        private elementRef: ElementRef, 
+        private elementRef: ElementRef,
         private renderer: Renderer2
     ) {}
-    
+
     @HostListener('mouseenter') onMouseEnter() {
-        this.showTooltip();
+        if (this.tooltipConfig.show) this.showTooltip();
     }
-    
+
     @HostListener('mouseleave') onMouseLeave() {
-        this.hideTooltip();
+      if (this.tooltipConfig.show) this.hideTooltip();
     }
-    
+
     private showTooltip() {
         if (!this.tooltipConfig.text || this.tooltipConfig.text == "") return;
-    
+
+        this.elementRef.nativeElement.classList.add("relative");
         this.tooltipElement = this.renderer.createElement('div');
         this.renderer.addClass(this.tooltipElement, 'tooltip');
         this.renderer.addClass(this.tooltipElement, `tooltip-${this.tooltipConfig.position}`);
@@ -41,6 +45,7 @@ export class TooltipDirective {
 
     private hideTooltip() {
         if (this.tooltipElement) {
+          this.elementRef.nativeElement.classList.remove("relative");
             this.renderer.removeChild(this.elementRef.nativeElement, this.tooltipElement);
             this.tooltipElement = null;
         }
