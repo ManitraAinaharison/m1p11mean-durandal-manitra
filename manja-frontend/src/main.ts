3033a7fd09154edd45532a27ebe606e1e17c7d8d
Anthony Durandal <a.durandal@outlook.com>
2024-02-27 23:26:16 +0300

    feat[pipes]: create shared pipes
    for date formatting and status formatting

--- manja-frontend/src/main.ts
+++ manja-frontend/src/main.ts
@@ -5,9 +5,17 @@ import { AppModule } from './app/app.module';
 import * as dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import calendar from 'dayjs/plugin/calendar';
+
+import 'dayjs/locale/fr';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.locale('fr')
+dayjs.extend(localizedFormat);
+dayjs.extend(calendar);
+
 
 dayjs.tz.setDefault(dayjs.tz.guess());
 
