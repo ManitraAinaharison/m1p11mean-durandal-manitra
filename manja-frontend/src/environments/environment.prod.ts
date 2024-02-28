b18ddd1e52a94f3074e11c9b27681c462d746e14
Anthony Durandal <a.durandal@outlook.com>
2024-02-29 02:04:05 +0300

    Update environment.prod.ts

--- manja-frontend/src/environments/environment.prod.ts
+++ manja-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // production environnement
 export const environment = {
   production: true,
-  baseUrl: 'https://m1p11mean-durandal-manitra-1.onrender.com/', // TODO: To be changed during production launch
+  baseUrl: 'https://m1p11mean-durandal-manitra-1.onrender.com', // TODO: To be changed during production launch
 };
   
